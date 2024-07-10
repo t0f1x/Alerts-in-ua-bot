c5d4cb83f66249bb6dc55058a8c6a705baec5b96
t0f1x <80917980+t0f1x@users.noreply.github.com>
2024-07-10 20:29:57 +0300

    Update Alarm_bot.py
    Added a database that stores information about the last alarm status in the area, and when restarting the bot does not write a new message to the channel if the alarm status is the same as before the restart

--- Alarm_bot.py
+++ Alarm_bot.py
@@ -1,26 +1,43 @@
-#project by t0f1x
-
-
 from telebot import *
 from alerts_in_ua import Client as AlertsClient
-from datetime import datetime
-bot = TeleBot("") #write you token (create bot in https://t.me/BotFather)
-alerts_client = AlertsClient(token="") #write you token (https://alerts.in.ua/api-request?)
+from datetime import datetime, timedelta
+import  sqlite3
+db = sqlite3.connect("Local.db", check_same_thread=False)
+sql = db.cursor()
+sql.execute("""CREATE TABLE IF NOT EXISTS Alarms (
+    Sended INT
+    )""")
+db.commit()
+sql.execute("SELECT Sended FROM Alarms")
+if sql.fetchone() is None:
+        sql.execute(f"INSERT INTO Alarms VALUES(?)",("T"))
+        db.commit()
+
+bot = TeleBot("!!!!!write you token (create bot in https://t.me/BotFather)") 
+alerts_client = AlertsClient(token="!!!!!!write you token (https://alerts.in.ua/api-request?)")
 def main(message):
-    chat_id = "00000000" #write here the id of the channel or chat in which you want to write about the air alarm (https://t.me/getmyid_bot) for find id(forward from channel for find channel id)
-    check = 1
+    chat_id = "!!!!!write here the id of the channel or chat in which you want to write about the air alarm (https://t.me/getmyid_bot) for find id(forward from channel for find channel id)" 
     while True:
-        now = datetime.now()
+        clock = datetime.now()
+        now = clock + timedelta(hours = 3) #depending on the difference between the host time and your local time (for example, on the server GMT 0, and local time GMT +3)
         current_time = now.strftime("%H:%M")
         active_alerts = alerts_client.get_active_alerts()
-        location_uid_alerts = active_alerts.get_alerts_by_location_uid('write you uid  (https://devs.alerts.in.ua/#modeluid)')
-        if location_uid_alerts != [] and check == 1:
-            bot.send_message(chat_id,f"text about the start of the air alarm {current_time}")
-            check = 0
-        elif location_uid_alerts == [] and check == 0:
-            bot.send_message(chat_id,f"text about the end of the air alarm {current_time}")
-            check = 1
-        time.sleep(60)
+        location_uid_alerts = active_alerts.get_alerts_by_location_uid('!!!!!write you uid  (https://devs.alerts.in.ua/#modeluid)')
+        sql.execute(f"SELECT Sended FROM Alarms")
+        check = sql.fetchone()
+        print(check[0])
+        if location_uid_alerts != [] and check[0] != "T":
+            bot.send_message(chat_id,f"!!!!!text about the start of the air alarm {current_time}")
+            chech = "T"
+            sql.execute(f"UPDATE Alarms SET Sended = '{chech}'")
+            db.commit()
+        elif location_uid_alerts == [] and check[0] != "F":
+            bot.send_message(chat_id,f"!!!!!text about the end of the air alarm {current_time}")
+            chech = "F"
+            sql.execute(f"UPDATE Alarms SET Sended = '{chech}'")
+            db.commit()
+        print("request allow")
+        time.sleep(8)
 @bot.message_handler(commands=["start"])
 def start(message):
     print("started by user")
