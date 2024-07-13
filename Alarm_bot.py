e6be317c9933c27076d650c3628093cba645741c
t0f1x <80917980+t0f1x@users.noreply.github.com>
2024-07-13 20:11:16 +0300

    Update Alarm_bot.py

--- Alarm_bot.py
+++ Alarm_bot.py
@@ -36,7 +36,7 @@ def main(message):
             sql.execute(f"UPDATE Alarms SET Sended = '{chech}'")
             db.commit()
         print("request allow")
-        time.sleep(12)
+        time.sleep(12) #If your server CPU-seconds are being used quickly, increase the time between requests
 @bot.message_handler(commands=["start"])
 def start(message):
     print("started by user")
